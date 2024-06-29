5f5fd23dac3ff42882fa4ba9665d037f828c1615
Gadubest <123305966+Gadubest@users.noreply.github.com>
2024-06-29 17:22:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
+const multer = require('multer');
+
+const upload = multer({ dest: 'uploads/' });
+
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+  });
+
+app.post('/upload', upload.single('image'), (req, res) => {
+    res.send('Received');
+})
